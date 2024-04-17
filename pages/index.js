fa42966473eccf4e287722e2145b1f8d369e2ec1
aya <aya.yamauchi@drobe.jp>
2024-04-17 06:22:42 +0200

    Add strict mode

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,27 @@
 import Head from "next/head";
-import React, { useState } from "react";
+import React, { useState, StrictMode } from "react";
 
-import { Inter } from 'next/font/google'
-import Words from './Words'
-import Genders from './Genders'
+import { Inter } from "next/font/google";
+import Words from "./Words";
+import Genders from "./Genders";
 
-const inter = Inter({ subsets: ['latin'],  variable: '--inter-font', })
+const inter = Inter({ subsets: ["latin"], variable: "--inter-font" });
 
 export default function Home() {
-
-  const [page, setPage] = useState('words');
+  const [page, setPage] = useState("words");
 
   return (
-    <div className={inter.className}>
-     <Head>
+    <StrictMode className={inter.className}>
+      <Head>
         <title>German words</title>
-        <meta
-          name="description"
-          content="learn 2000 German words"
-        />
+        <meta name="description" content="learn 2000 German words" />
         <meta name="viewport" content="width=device-width, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {page === "words" &&  <Words onChangePage={(value) => setPage(value)} /> }
-      {page === "genders" &&  <Genders onChangePage={(value) => setPage(value)} />}
-    </div>
+      {page === "words" && <Words onChangePage={(value) => setPage(value)} />}
+      {page === "genders" && (
+        <Genders onChangePage={(value) => setPage(value)} />
+      )}
+    </StrictMode>
   );
 }
